4764af841439f72b03ae86155fadfc549152eb7e
unknown <ingrid.chan92@gmail.com>
2015-01-17 12:36:53 -0500

    scripts

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	public int senseDistance;
+	public int stalkDistance;
+	public int chaseDistance;
+	public int huntDistance;
+	
+	public float senseSpeed;
+	public float stalkSpeed;
+	public float chaseSpeed;
+	public float huntSpeed;
+	
+	private GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt (player.transform);
+			
+		float distance = Vector3.Distance (player.transform.position, transform.position);
+		if (distance <= huntDistance) {
+			transform.position += transform.forward * huntSpeed * Time.deltaTime;
+		} else if (distance <= chaseDistance) {
+			transform.position += transform.forward * chaseSpeed * Time.deltaTime;
+		} else if (distance <= stalkDistance) {
+			transform.position += transform.forward * stalkSpeed * Time.deltaTime;
+		} else if (distance <= senseDistance) {
+			transform.position += transform.forward * senseSpeed * Time.deltaTime;
+		} else {
+			// Don't move
+		}
+	}
+
+	void Die() {
+		this.gameObject.SetActive (false);
+		Destroy (this.gameObject);
+	}
+
+}
---
+++ Assets/scripts/SpawnScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject enemy;
+	public int maxEnemy;
+	
+	private GameObject[] spawnpoints;
+	private float lastEnemySpawnTime;
+	
+	// Use this for initialization
+	void Start () {
+		lastEnemySpawnTime = 0;
+		spawnpoints = GameObject.FindGameObjectsWithTag("spawnpoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Spawn enemy if not enough on screen
+		float diff = Time.time - lastEnemySpawnTime;
+		
+		if (diff > 2.0F && GameObject.FindGameObjectsWithTag ("enemy").Length < maxEnemy) {
+			Instantiate (enemy, spawnpoints[Random.Range(0, 14)].transform.position, Quaternion.identity);
+			lastEnemySpawnTime = Time.time;
+		}
+	}
+}
