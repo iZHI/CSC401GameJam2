fe71b61712695f599e4b71bfd8903226aefdb894
osdoshk <osdoshk@gmail.com>
2015-01-17 12:56:27 -0500

    Added Gun Light Script

---
+++ Assets/scripts/GunLightScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunLightScript : MonoBehaviour {
+	public float hitdistance;
+
+	void Start() {
+		hitdistance = 100.0f;
+	}
+
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, hitdistance)) {
+			Destroy(hit.transform.gameObject);
+		}
+	}
+}
